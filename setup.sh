0a0adc9476f1427b2307e601970ceab916fa79f1
michael <micre@protonmail.com>
2023-12-30 16:18:35 +0100

    init

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script sets up the boring parts of an arch install
+#
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+sed -i '133s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1" >> /etc/hosts
+echo "::1" >> /etc/hosts
+echo "127.0.0.1 arch.localdomain arch" >> /etc/hosts
+
+pacman -Sy linux-headers openssh xorg webkit2gtk # correct drivers for your graphics card, check wiki if Optimus is needed
+systemctl enable NetworkManager
+systemctl enable reflector.timer
+
+useradd -m -G wheel -s /bin/bash michael
+# sed -i '85s/.//' /etc/sudoers  # risky, could also be 82
+
+
